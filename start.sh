ea914a9bfff2abce4a61a8f1633b246dccf9686a
nardis556 <82609175+nardis556@users.noreply.github.com>
2022-11-10 14:43:05 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 
-source /home/$USER/velodyne_driver/devel/setup.bash
-source /home/$USER/catkin_ws/devel/setup.bash
-source /home/$USER/usb_camera/devel/setup.bash
+bash -c source /home/$USER/velodyne_driver/devel/setup.bash
+bash -c source /home/$USER/catkin_ws/devel/setup.bash
+bash -c source /home/$USER/usb_camera/devel/setup.bash

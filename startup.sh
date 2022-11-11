1b414a3c9287f728cdbae5406221d5e136b863d4
nardis556 <82609175+nardis556@users.noreply.github.com>
2022-11-10 20:18:12 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
 #!bin/bash
 
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-echo "source /home/$USER/catkin_ws/devel/setup.bash" >> ~/.bashrc
+function start() {
+    bash -c 'source /opt/ros/noetic/setup.bash'
+    bash -c 'source /home/$USER/catkin_ws/devel/setup.bash'
+}
+
+start()
 
 source ~/.bashrc

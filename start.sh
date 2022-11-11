edd6205fa1fd3a4bf8e94b25d58443e401c18ac7
nardis556 <82609175+nardis556@users.noreply.github.com>
2022-11-10 20:14:09 -0600

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-bash -c source /home/$USER/velodyne_driver/devel/setup.bash
-bash -c source /home/$USER/catkin_ws/devel/setup.bash
-bash -c source /home/$USER/usb_camera/devel/setup.bash
-bash -c source /opt/ros/noetic/setup.bash

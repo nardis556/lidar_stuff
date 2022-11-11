4437644f6b6238a65352a57dc0df2e66119899b7
nardis556 <82609175+nardis556@users.noreply.github.com>
2022-11-10 18:08:24 -0600

    Update before_opencv.sh

--- before_opencv.sh
+++ before_opencv.sh
@@ -2,6 +2,6 @@
 
 sudo add-apt-repository 'deb http://security.ubuntu.com/ubuntu xenial-security main'
 sudo apt update
-sudo apt install libjasper1 libjasper-dev
+sudo apt install libjasper1 libjasper-dev libpcap0.8-dev
 
 sudo apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev

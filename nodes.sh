d4cebdf2baf4ee0b45056d436b24ef826a5ff4e3
nardis556 <82609175+nardis556@users.noreply.github.com>
2022-11-13 18:39:34 -0600

    Update nodes.sh

--- nodes.sh
+++ nodes.sh
@@ -2,6 +2,7 @@
 
 function start() {
     bash -c 'x-terminal-emulator -e roscore'
+    bash -c 'sleep 5'
     bash -c 'x-terminal-emulator -e roslaunch velodyne_pointcloud 32e_points.launch'
     bash -c 'x-terminal-emulator -e roslaunch usb_cam usb_cam-test.launch'
     bash -c 'x-terminal-emulator -e rosrun velodyne_gps_imu gpsimu_driver'
